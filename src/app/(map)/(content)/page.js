c889cf535fb2b8bb94965f1fe6bd8024bd4072f1
Duncan Hay <d.hay@ucl.ac.uk>
2023-09-05 12:04:17 +0100

    change page caching

--- src/app/(map)/(content)/page.js
+++ src/app/(map)/(content)/page.js
@@ -1,7 +1,7 @@
 import { sanitize, isSupported } from 'isomorphic-dompurify'
 
 async function getPage() {
-    const res = await fetch(process.env.MEMORYMAPPER_ENDPOINT + '2.0/pages/front/')
+    const res = await fetch(process.env.MEMORYMAPPER_ENDPOINT + '2.0/pages/front/', {cache: 'no-cache'})
 
     if (!res.ok) {
         throw new Error('Failed to fetch page')
@@ -18,7 +18,7 @@ export default async function Page() {
     const clean = sanitize(page.body)
 
     return (
-        <div className="w-full">
+        <div className="w-full h-full overflow-auto">
             <h1>{ page.title }</h1>
             <div dangerouslySetInnerHTML={{__html: clean}}></div>
         </div>
