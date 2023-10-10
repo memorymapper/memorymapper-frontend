df38129ed5ac384ea62fe38fbc203ca58ee56bba
ddunc23 <d.hay@ucl.ac.uk>
2023-10-10 14:42:58 +0100

    titles, images

--- src/app/(map)/(content)/page/[slug]/TabContent.js
+++ src/app/(map)/(content)/page/[slug]/TabContent.js
@@ -18,35 +18,44 @@ export default function TabContent(props) {
 
     return (
         <>
-            <ContentPanelSelect tabs={props.tabs} />
-                <div className="hidden sm:block">
-                    <div className="border-b border-gray-200">
-                        <nav className="-mb-px flex space-x-8" aria-label="Tabs">
-                            {activeTabs.map((tab) => (
-                            <button
-                                key={tab.name}
-                                id={tab.name}
-                                className={classNames(
-                                tab.current
-                                    ? 'border-gray-500 text-indigo-600'
-                                    : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                                'whitespace-nowrap border-b-2 py-4 px-1 text-sm font-light'
-                                )}
-                                aria-current={tab.current ? 'page' : undefined}
-                                style={{color: tab.color}}
-                                onClick={handleClick}
-                            >
-                                {tab.name}
-                            </button>
-                            ))}
-                        </nav>
+            {activeTabs ? activeTabs.map((tab) => (
+                    <div className={tab.current ? 'block' : 'hidden'} key={tab.name}>
+                        <h1>{tab.name}</h1>
                     </div>
-                </div>
+                ) 
+            ) : null}
+            {activeTabs.length > 1 ? (
+                <>
+                    <ContentPanelSelect tabs={props.tabs} />
+                    <div className="hidden sm:block">
+                        <div className="border-b border-gray-200">
+                            <nav className="-mb-px flex space-x-8" aria-label="Tabs">
+                                {activeTabs.map((tab) => (
+                                <button
+                                    key={tab.name}
+                                    id={tab.name}
+                                    className={classNames(
+                                    tab.current
+                                        ? 'border-gray-500 text-indigo-600'
+                                        : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
+                                    'whitespace-nowrap border-b-2 py-4 px-1 text-sm font-light'
+                                    )}
+                                    aria-current={tab.current ? 'page' : undefined}
+                                    style={{color: tab.color}}
+                                    onClick={handleClick}
+                                >
+                                    {tab.name}
+                                </button>
+                                ))}
+                            </nav>
+                        </div>
+                    </div>
+                </>
+            ) : null}
             <div>
                 {activeTabs ? activeTabs.map((tab) => (
                     <div className={tab.current ? 'block' : 'hidden'} key={tab.name}>
-                        <h1>{tab.title}</h1>
-                        <div dangerouslySetInnerHTML={{__html: tab.body}}></div>
+                        <div className='mt-6' dangerouslySetInnerHTML={{__html: tab.body}}></div>
                     </div>
                     )
                 ) : null
